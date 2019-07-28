978ecc54e926146b964262dfbf3c2ce20589dbe8
Alvin Wong <alvinwong312@gmail.com>
2019-07-28 00:12:32 -0400

    only load download.js on page load

--- popup.js
+++ popup.js
@@ -1,8 +1,5 @@
 function injectTheScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    // query the active tab, which will be only one tab
-    //and inject the script in it
-    chrome.tabs.executeScript(tabs[0].id, {file: "download.js"});
     var downloadUrl = document.getElementById("download-url").value;
     if (downloadUrl == null || downloadUrl == undefined || downloadUrl == ""){
         var blob = new Blob([ "test"], {type : "text/plain;charset=UTF-8"});
@@ -17,5 +14,6 @@ document.getElementById('download-btn').addEventListener('click', injectTheScrip
 
 chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
     document.getElementById('download-url').value = tabs[0].url;
+    chrome.tabs.executeScript(tabs[0].id, {file: "download.js"});
 });
 
