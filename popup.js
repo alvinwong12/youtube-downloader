dc30941f746e3aa6ebbd8ea1de770167877c00df
Alvin Wong <alvinwong312@gmail.com>
2019-07-28 00:13:18 -0400

    default download current tab url

--- popup.js
+++ popup.js
@@ -2,8 +2,7 @@ function injectTheScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var downloadUrl = document.getElementById("download-url").value;
     if (downloadUrl == null || downloadUrl == undefined || downloadUrl == ""){
-        var blob = new Blob([ "test"], {type : "text/plain;charset=UTF-8"});
-        downloadUrl = window.URL.createObjectURL(blob);
+        downloadUrl = tabs[0].url;
     }
     chrome.tabs.sendMessage(tabs[0].id, {downloadUrl: downloadUrl});
 });
