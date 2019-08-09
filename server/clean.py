0170eec3990c7e7b1b6c7552df7d3d011cb4721c
Alvin Wong <alvinwong312@gmail.com>
2019-08-08 21:38:00 -0400

    Extend wait to 10s

--- server/clean.py
+++ server/clean.py
@@ -6,10 +6,13 @@ class Clean(object):
 
     @staticmethod
     def remove(filename):
-        time.sleep(5)
-        os.remove(filename)
+        time.sleep(10)
+        try:
+            os.remove(filename)
+        except: pass
+        finally: return
 
     @staticmethod
     def scheduleRemove(filename):
-        t = threading.Thread(target=Clean.remove args=filename)
+        t = threading.Thread(target=Clean.remove, args=[filename])
         t.start()
